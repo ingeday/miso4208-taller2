c6452a16bbe49e9be80d361c5989985b5dc389e7
Eday Gonzalez <edaygonzalez@ingenieros.com>
2020-08-30 21:30:01 -0500

    Proyecto de cypress y protractor

---
+++ cypress/cypress/integration/simple.spec.js
@@ -0,0 +1,13 @@
+context('Login Tests', ()=>{
+    it('makes a wrong login attemp', ()=>{
+        cy.visit('https://habitica.com/static/home');
+        cy.get('.login-button').click();
+
+        cy.get('#usernameInput').type('fake@email.com').should('have.value', 'fake@email.com');
+        cy.get('#passwordInput').type('fake@email.com');
+
+        cy.get('.btn-info[type="submit"]').click();
+
+        cy.contains("your email address / username or password is incorrect").should('be.visible');
+    })
+})
